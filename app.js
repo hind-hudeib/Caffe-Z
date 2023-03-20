dde0473f137e8695e96cd0e1185f3e83d74e42a5
Hind Hudeib <xxhindsaed27@gmail.com>
2023-03-20 02:12:13 -0700

    change html file

--- app.js
+++ app.js
@@ -3,36 +3,35 @@ let person = prompt("Please enter your name");
 let gender = prompt("Please enter your gender");
 
 
+function genderName(gender1) {
+    while (gender1 != "female" && gender1 != "male") {
 
-    while(gender!= "female" && gender != "male")
-    {
-
-         gender = prompt("Please enter your gender correctly : ");
+        gender1 = prompt("Please enter your gender correctly : ");
 
 
     }
 
-    
+
     if (gender == "female") {
         alert("Hello Mrs " + person);
     }
     else if (gender == "male") {
         alert("Hello Mr " + person);
-      }
-    
+    }
+}
 
-
+genderName(gender)
 // -----------------------------------------------------------------
 
 let drink = prompt("Do you want a hot or cold drink ");
 
 let drinkName = prompt("What do you want to drink?");
 
-alert(drink +" " + drinkName + " is getting prepared");
+alert(drink + " " + drinkName + " is getting prepared");
 
-console.log(person +" "+ drinkName);
+console.log(person + " " + drinkName);
 
-let customerInfo = [person , gender , drink , drinkName];
+let customerInfo = [person, gender, drink, drinkName];
 
 for (var i = 0; i < customerInfo.length; i++) {
     console.log(customerInfo[i]);
